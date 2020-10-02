0f1b370289e54f7d4682449b000f6ca0f8bc3846
Stud. Camilla Pittatore <c.pittatore.1002@vallauri.net>
2020-10-02 12:59:58 +0200

    randomName aggiunto

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  names=["sono bello","sono figo","sono fotomodello","sono denis dosio","levati la corona","indossa l'armatura","roberta è scontrosa","edoardo rompe","filippo II",];
+  myName= 'Camilla Pttatore';
+  
+
+  changeName(){
+    this.myName=this.names[Math.floor(Math.random()*9)];
+  }
 }
+
