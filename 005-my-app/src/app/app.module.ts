4a5929f307b16a9530feb9a43987e91439f665c3
Stud. Camilla Pittatore <c.pittatore.1002@vallauri.net>
2020-10-16 13:00:56 +0200

    16/10/2020

--- 005-my-app/src/app/app.module.ts
+++ 005-my-app/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
-
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
+import { ServerComponent } from './server/Server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
